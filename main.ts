ef516b2a69e02727ca2f16eb0c3bee4108c12503
Dominic Dopico <serafin.dopico@gmail.com>
2020-03-30 17:17:26 -0700

    class instances and objects

--- main.ts
+++ main.ts
@@ -1,22 +1,10 @@
 
-//interfaces are prefered
-interface Point {
-  x: number,
-  y: number
-}
-
-//now point is it's own custom type
-let drawPoint = (point: Point) => {
-  // ...
-}
-
-//but these two should live together according to cohesion principle we need a class instead
 class Point {
   x: number;
   y: number;
 
   draw() {
-    // ...
+    console.log(`X: ${this.x}, Y: ${this.y}`);
   }
 
   getDistance(another: Point) {
@@ -24,6 +12,12 @@ class Point {
   }
 }
 
-//now all like things live together
-//like in other strongy typed languages, functions in a class are called methods
-//no need to pass point obj param to methods because they can access the point obj properties nativel
+// create new point object
+let point1 = new Point;
+
+// assign values
+point1.x = 1;
+point1.y = 2;
+
+// draw new point
+point1.draw();
