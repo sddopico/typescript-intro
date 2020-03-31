39c5990b4c267d1e65768cc5ada7ee9244a9364c
Dominic Dopico <serafin.dopico@gmail.com>
2020-03-31 09:50:44 -0700

    exercise answer

---
+++ exercise/button.ts
@@ -0,0 +1,40 @@
+// define a ts class w/ the right members
+// allow consumer to pass initial number of likes
+// define in separate module. simulate scenario where the user
+// clicks the component. display total #of likes and whether the
+// button is in the selected or unselected state on the console
+
+export class LikeButton {
+
+  constructor(private _likes?: number, private _selButton?: boolean) {
+    _selButton = false;
+  }
+
+  pressButton() {
+    if (this._selButton == true) {
+      this._selButton = !this._selButton;
+      this._likes--;
+      console.log(`${this._likes} likes, Button selected?: ${this._selButton}`);
+    } else {
+      this._selButton = !this._selButton;
+      this._likes++;
+      console.log(`${this._likes} likes, Button selected?: ${this._selButton}`);
+    }
+  }
+
+  get likes() {
+    return this._likes;
+  }
+
+  set likes(value) {
+    if (value > 0) {
+      throw new Error('Likes value must be greater than 0')
+    }
+    this._likes = value;
+  }
+}
+
+let button = new LikeButton(3);
+
+button.pressButton();
+button.pressButton();
